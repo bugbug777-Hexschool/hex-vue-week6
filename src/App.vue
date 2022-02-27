4714a54e875bd54d65b172caf47a85ad77b7901b
斯勒黃 <sihle@sileihuangdeiMac.local>
2022-02-27 14:58:25 +0800

    [add] add cutsomized bootstrap and vue-axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <button type="button" class="btn btn-primary">Sure</button>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -7,24 +8,5 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '@/assets/all';
 </style>
