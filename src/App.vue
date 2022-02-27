85405570a2d6bb6a97082542711571fd6a2b6833
sihle <gubug777@gmail.com>
2022-02-27 15:16:31 +0800

    [add] add Views FrontView and DashboardView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <button type="button" class="btn btn-primary">Sure</button>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/admin">Dashboard</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
