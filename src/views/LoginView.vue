7cfefcf14b7dea4b71d1d1d8f9ed961ceb488c76
sihle <gubug777@gmail.com>
2022-02-27 19:49:48 +0800

    [add] add data validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,33 +1,42 @@
 <template>
   <div class="container | p-3 pt-5">
-    <form @submit.prevent="login">
+    <v-form @submit="login" v-slot="{ errors }">
       <div class="row justify-content-center">
         <div class="col-4">
           <h1 class="text-center">使用者請登入</h1>
-          <div class="mb-3">
-            <label for="email" class="d-block"
-              ><span class="fs-5 | mb-1">帳號</span>
-              <input
-                v-model="user.username"
-                type="email"
-                class="form-control"
-                id="email"
-                aria-describedby="emailHelp"
-              />
-            </label>
+          <div class="form-floating mb-3">
+            <v-field
+              v-model="user.username"
+              type="email"
+              name="email"
+              class="form-control"
+              :class="{ 'is-invalid': errors['email'] }"
+              id="email"
+              rules="email|required"
+              aria-describedby="example@.mail.com"
+            ></v-field>
+            <label for="email" class="d-block">帳號</label>
+            <error-message name="email" class="invalid-feedback"></error-message>
           </div>
-          <div class="mb-3">
-            <label for="password" class="d-block"
-              ><span class="fs-5 | mb-1">密碼</span>
-              <input v-model="user.password" type="password" class="form-control" id="password" />
-            </label>
+          <div class="form-floating mb-3">
+            <v-field
+              v-model="user.password"
+              type="password"
+              name="password"
+              class="form-control"
+              rules="required"
+              :class="{ 'is-invalid': errors['password'] }"
+              id="password"
+            ></v-field>
+            <label for="password" class="d-block">密碼</label>
+            <error-message name="password" class="invalid-feedback"></error-message>
           </div>
           <div class="text-end">
             <button type="submit" class="btn btn-primary">登入</button>
           </div>
         </div>
       </div>
-    </form>
+    </v-form>
   </div>
 </template>
 
