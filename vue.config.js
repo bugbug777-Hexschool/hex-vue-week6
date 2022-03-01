8be01b2ec78661e00df478ccd5fe607ebefa1379
sihle <gubug777@gmail.com>
2022-03-01 11:23:31 +0800

    ready to deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './hex-vue-week6/dist/',
+  publicPath: './',
 });
